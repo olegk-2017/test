fc38f85bc146423a4b256f27e6d4c6c6a568ba5d
Oleg <olegkovalinsky@gmail.com>
2021-12-25 21:54:44 +0200

    add html

--- webServ/index.js
+++ webServ/index.js
@@ -1,16 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader("UserId", 12);
-    res.setHeader("Content-Type", "text/html; charset=utf-8;");
-    res.write("<body><h2>Hello word</h2></body>");
-    res.write("</html>");
-    res.end();
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/3.html', 'utf8');
+    myReadStream.pipe(response);
 });
 
 
