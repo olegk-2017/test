2ada5da92e050d800ac292d96798f73952603938
olegk-2017 <olegkovalinsky@gmail.com>
2021-12-25 21:42:25 +0200

    change message

--- webServ/index.js
+++ webServ/index.js
@@ -8,10 +8,11 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader("UserId", 12);
     res.setHeader("Content-Type", "text/html; charset=utf-8;");
-    res.write("<body><h2>Привет миг</h2></body>");
+    res.write("<body><h2>Hello word</h2></body>");
     res.write("</html>");
     res.end();
 });
+
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
